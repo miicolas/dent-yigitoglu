2934e985ebf3e2d8d7e6eaa051c2a11c20aa634f
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-06-13 21:56:19 +0200

    ⚙️

---
+++ src/data/socialMediaData.js
@@ -0,0 +1,16 @@
+const navData = [
+    {
+        name : "Instagram",
+        path : "",
+        svg : "fa-brands fa-instagram"   
+    },
+    {
+        name : "LeMédecin",
+        path : "https://lemedecin.fr/paris/selarl-dr-thierry-yigitoglu/dentiste/docteur-yigitoglu-thierry-mehmet/ad3b1f241c18c76a90a635be1de66320/pro/",
+        svg :"fa-solid fa-stethoscope"
+    },
+    
+    
+]
+
+export default navData;
